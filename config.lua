02c92277638f02645a88fa08d70df54bdf687605
Neo <79176755+NeoKSA@users.noreply.github.com>
2022-10-17 19:32:12 +0300

    add job event and you can add more box zones

--- config.lua
+++ config.lua
@@ -1,16 +1,19 @@
 Config = {}
 
-Config.Core = 'qb-core';
-Config.input = 'qb-input';
+Config.coreExport = 'qb-core';
+Config.inputExport = 'qb-input';
+Config.targetExport = 'qb-target';
+
 Config.Target = {
-    target = 'qb-target',
-    coords = vec3(458.81, -990.09, 24.91),
-    a = 2.45,
-    b = 0.7,
-    heading = 0,
-    debugPoly = true,
-    minZ = 23.91,
-    maxZ = 26.36,
-    job = 'police',
-    distance = 1.0
+    {
+        coords = vec3(458.81, -990.09, 24.91),
+        a = 2.45,
+        b = 0.7,
+        heading = 0,
+        debugPoly = true,
+        minZ = 23.91,
+        maxZ = 26.36,
+        job = 'police',
+        distance = 1.0
+    },
 }
