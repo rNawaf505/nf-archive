4d1dd591c7431eb122fd9b4716d6e568400a8ef6
Neo <79176755+NeoKSA@users.noreply.github.com>
2022-10-17 14:05:09 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Author : Nawaf505#0772
+-- Discord : https://discord.gg/AM1
+-- if discord invite not working : https://discord.gg/wr5G9hSvME
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_script 'config.lua'
+client_script 'client.lua'
